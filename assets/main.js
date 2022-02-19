3d9c301d1c8502a7d018e440692f0cf8e3907fdd
Salvation27 <nikolayzalipykin@gmail.com>
2022-02-20 00:07:46 +0200

    fist commit

---
+++ assets/main.js
@@ -0,0 +1,24 @@
+const socket = io();
+const messages = document.querySelector(".messages");
+const form = document.querySelector(".form");
+const input = document.querySelector(".input");
+const nameBlock = document.querySelector(".name");
+
+const userName = prompt("Введите имя");
+
+nameBlock.innerHTML = `${userName}`;
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (input.value) {
+    socket.emit("chat message", { message: input.value, name: userName });
+    input.value = "";
+  }
+});
+
+socket.on("chat message", (data) => {
+  const item = document.createElement("li");
+  item.innerHTML = `<span>${data.name}</span>:${data.message}`;
+  messages.appendChild(item);
+  window.scrollTo(0,document.body.scrollHeight)
+});
